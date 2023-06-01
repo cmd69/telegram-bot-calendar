bbf4dcc85983d6ce3567398936fcd4133be6203e
cmd69 <nasamigra@gmail.com>
2023-06-01 18:19:03 +0200

    New calendar for selecting year and month only.

--- telegram_bot_calendar/__init__.py
+++ telegram_bot_calendar/__init__.py
@@ -1,3 +1,4 @@
 from telegram_bot_calendar.detailed import DetailedTelegramCalendar, YEAR, MONTH, DAY, LSTEP
+from telegram_bot_calendar.month import MonthTelegramCalendar, YEAR, MONTH, DAY, LSTEP
 from telegram_bot_calendar.wmonth import WMonthTelegramCalendar
 from telegram_bot_calendar.wyear import WYearTelegramCalendar
